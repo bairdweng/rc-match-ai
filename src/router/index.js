bb110cab297c9fda33b99ae516a199b9d908ff74
bairdweng <wengxianjiang@miya818.com>
2025-11-03 23:41:48 +0800

    fix: 修复Vue路由配置，使用Hash模式适配GitHub Pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import SearchResults from '../components/SearchResults.vue'
 
@@ -17,7 +17,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
